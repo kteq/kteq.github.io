36eae6c6d2528209bd98d32e982cd0bcd2908c07
Julian Brackins <julian.brackins@mines.sdsmt.edu>
2016-09-24 09:32:23 -0600

    Added mobile site as well.

--- scripts/more.js
+++ scripts/more.js
@@ -4,12 +4,12 @@ $(document).ready(function(){
   "<a class='dropdown-toggle' data-toggle='dropdown' href='#'>MORE" +
   "        <span class='caret'></span></a>" +
   "        <ul class='dropdown-menu'>" +
-  "          <li><a href='#'>Current Programs</a></li>" +
-  "          <li><a href='#'>Upcoming Events</a></li>" +
-  "          <li><a href='#'>Blog</a></li>" +
+ // "          <li><a href='#'>Current Programs</a></li>" +
+ // "          <li><a href='#'>Upcoming Events</a></li>" +
+ // "          <li><a href='#'>Blog</a></li>" +
   "          <li><a href='history.html'>History</a></li>" +
   "          <li><a href='forms.html'>Forms and Documentation</a></li>" +
-  "          <li><a href='#'>Management</a></li>" +
+  "          <li><a href='management.html'>Management</a></li>" +
   "          <li><a target='_blank' href='https://sdsmt.collegiatelink.net/organization/kteq'>KTEQ Mines Link Page</a></li>" +
   "        </ul>";
 })
